b7525097dd745e14d5f14a6d00437704b0cec807
Yorkd1 <davidgyork@gmail.com>
2024-03-05 14:21:03 -0700

    Completed learning activity for W02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // job1
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Microsoft";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+        job1.Display();
+
+        // job2
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._companyName = "Apple";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
+        job2.Display();
+
+        // Resume
+        Resume resume1 = new Resume();
+        resume1._name = "David York";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
